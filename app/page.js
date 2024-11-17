f38c603adaec2abe41abec835994b93a77fa86cc
Rajaruban Rajindram <85555894+rubanero14@users.noreply.github.com>
2024-11-17 19:04:46 +0800

    Create functionality and data persistence setup

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 "use client";
-import { useState } from "react";
+import { memo, useState, useEffect } from "react";
 
-export default function Home() {
+function Home() {
   // CRUD NAME, EMAIL, PHONE FIELDS
   const [address, setAddress] = useState([]);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
-
-  // useEffect(() => {}, []);
 
   const changeHandler = (e) => {
     switch (e.target.name) {
@@ -22,31 +20,67 @@ export default function Home() {
         setPhone(e.target.value);
         break;
       default:
-        break;
+        return;
     }
-    console.log(name, email, phone);
   };
 
   // Create
   const createAddress = (e) => {
     e.preventDefault();
-    setAddress((prev) => [
-      ...prev,
-      {
-        id: address.length + 1,
-        name: name,
-        email: email,
-        phone: phone,
-      },
-    ]);
-    localStorage.setItem("address-book", JSON.stringify(address));
+    if (address) {
+      setAddress([
+        ...address,
+        {
+          id: new Date(),
+          name: name,
+          email: email,
+          phone: phone,
+        },
+      ]);
+      localStorage.setItem(
+        "address-book",
+        JSON.stringify([
+          ...address,
+          {
+            id: new Date(),
+            name: name,
+            email: email,
+            phone: phone,
+          },
+        ])
+      );
+    } else {
+      setAddress([
+        {
+          id: new Date(),
+          name: name,
+          email: email,
+          phone: phone,
+        },
+      ]);
+      localStorage.setItem(
+        "address-book",
+        JSON.stringify([
+          {
+            id: new Date(),
+            name: name,
+            email: email,
+            phone: phone,
+          },
+        ])
+      );
+    }
+    console.log(address);
+
     getAddress();
   };
 
   // Read
   const getAddress = () => {
-    e.preventDefault();
-    setAddress(JSON.parse(localStorage.getItem("address-book")));
+    const data = localStorage.getItem("address-book");
+    if (data !== undefined || data.length > 0) {
+      setAddress([...JSON.parse(data)]);
+    }
   };
 
   // Update
@@ -64,10 +98,17 @@ export default function Home() {
             },
           ]
         );
+        localStorage.setItem(
+          "address-book",
+          JSON.stringify(...address, {
+            [add.name]: name,
+            [add.email]: email,
+            [add.phone]: phone,
+          })
+        );
       }
     });
 
-    localStorage.setItem("address-book", JSON.stringify(address));
     getAddress();
   };
 
@@ -76,12 +117,18 @@ export default function Home() {
     e.preventDefault();
     setAddress(...address.filter((add) => add.id !== id));
 
-    localStorage.setItem("address-book", JSON.stringify(address));
+    localStorage.setItem(
+      "address-book",
+      JSON.stringify(...address.filter((add) => add.id !== id))
+    );
     getAddress();
   };
+
+  useEffect(() => getAddress(), []);
+  console.log(address);
   return (
-    <main className="d-flex align-items-center justify-content-center py-5 px-3">
-      <div className="card text-center p-3">
+    <main className="d-flex align-items-center justify-content-center flex-column py-5 px-3">
+      <div className="card text-center mb-3 p-3">
         <h3 className="text-secondary title">Create New Address</h3>
         <form onSubmit={createAddress}>
           <input
@@ -112,6 +159,21 @@ export default function Home() {
           />
         </form>
       </div>
+      {address && address.length > 0 && (
+        <div className="card text-center p-3">
+          <h3 className="text-secondary title">Addresses</h3>
+          <ul>
+            {address &&
+              address.map((add) => (
+                <li key={add.id}>
+                  Name: {add.name} \n Email: {add.email} \n Phone: {add.phone}
+                </li>
+              ))}
+          </ul>
+        </div>
+      )}
     </main>
   );
 }
+
+export default memo(Home);
